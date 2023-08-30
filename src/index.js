dbb0654b47c65a01482741a0761071f9a7a47150
Wilmar Caicedo <wilmar.caicedo@farmappweb.com>
2023-08-30 16:48:56 -0500

    feat: add Dockerfile

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import { createPool } from 'mysql2/promise';
+
+const app = express();
+
+const pool = createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '123456',
+  port: 3307
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.get('/ping', async (req, res) => {
+  const result = await pool.query('SELECT NOW()');
+  res.json(result[0]);
+});
+
+app.listen(3000);
+console.log('Server on port', 3000);
